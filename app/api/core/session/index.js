1f2bcb0be610fd16664d92c3a05850a3d499ac26
BenoitRanque <ranque.benoit@gmail.com>
2018-01-30 06:13:03 -0400

    corrected sql require stament in login api

--- app/api/core/session/index.js
+++ app/api/core/session/index.js
@@ -1,13 +1,11 @@
 import express from 'express'
 
-import login from './login.sql'
-
 const router = express.Router()
 
 console.log(login)
 
 router.post('/login', function (req, res) {
-  req.pg.any(login, { username: req.body.username })
+  req.pg.any(require('./login.sql'), { username: req.body.username })
   .then(data => {
     console.log(data)
   })
